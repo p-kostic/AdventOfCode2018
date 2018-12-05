42f81cdd5715489760dd10a644796252893effea
Petar Kostic <snookik@gmail.com>
2018-12-05 20:33:48 +0100

    Finished Day 5 + small cleanup

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,15 +1,43 @@
 with open('input.txt') as f:
-    content = f.readlines()
+    content = f.read()
 
 example = "dabAcCaCBAcCcaDA"
 
-# Find matching pairs
+# Part 1
+def removeUnits(polymer):
+    initLength = len(polymer)
 
-# Remove recursively 
+    for unit in polymer:
+        polymer = polymer.replace(unit.upper() + unit.lower(), '').replace(unit.lower() + unit.upper(), '')
+    return initLength != len(polymer), polymer
 
-# after each recursive call, check if length still matches
+# removeUnits until fixed point is reached
+changed = False
+while not changed: 
+    changed, content = removeUnits(content)
 
-def funcname(polymer):
-    for (unit in polymer):
-        if(unit == polymer[unit+1].upper()):
-            polymer = polymer.replace(unit)
+print(content)
+print(len(content))
+
+
+# Part 2 
+# Reread input data
+with open('input.txt') as f:
+    content = f.read()
+
+# Current min Length
+minLength = len(content)
+# Set of input data (presumably) contains alphabet :p
+for char in set(content.lower()):
+
+    # remove upper and lowerspace occurences of characters of the data for this char
+    temp = content.replace(char.lower(), '').replace(char.upper(), '')
+
+    # Call removeUnits until fixed point is reached
+    changed = False
+    while not changed:
+        changed, temp = removeUnits(temp)
+    minLength = min(len(temp), minLength)
+
+print(minLength)
+
